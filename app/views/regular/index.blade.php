40bb43a8e54dcd87f305c15440085683b83014fe
Keita Goto <lw.mjw.kok@gmail.com>
2014-10-03 20:52:13 +0900

    申し込みページのデザイン適用と、エラーメッセージの表示 refs #17

--- app/views/regular/index.blade.php
+++ app/views/regular/index.blade.php
@@ -1,156 +1,342 @@
 @extends('layouts.default')
 
 @section('content')
+<header id="lpContents">
+	<div class="lpLogo">
+		<h1><img src="/images/lp/logo.png" width="462" height="84"></h1>
+		<a href="#form"><img src="/images/lp/teikiBtn.png"></a>
+	</div>
+	<div class="lptop">
+		<img src="/images/lp/lpImg01.png" width="723" height="168">
+		<div class="lptopIn">
+			<p>福岡の自社農園や九州各地の提携農家が、手をかけて育てた<span class="red">安心</span>な<span class="red">おいしい野菜</span>をあなたの食卓に届けます。過剰な肥料を利用することなく、また無理な作付けをすることもなく<span class="red">自然な環境</span>で育てられた旬の野菜は、<span class="red">栄養価も高く葉っぱまで</span>余すことなく食べることができます。</p>
+		</div>
+	</div>
+</header>
 
-{{ Form::open(['url' => '/company/contact/confirm', 'class' => 'js-register-form']) }}
-<table>
-	<tbody>
-		<tr>
-			<td class="tdLeft">ご希望のセット<br><span class="requiredArea">【必須】</span></td>
-			<td class="tdRight">
-				<p id="spryselect_area">
-					<select name="set">
-						<option value="" selected="">選択してください</option>
-						@foreach ($sets as $set)
-						<option value="{{{ $set['key'] }}}">{{{ $set['value'] }}}</option>
-						@endforeach
-					</select>
-			</p></td>
-		</tr>
-		<tr>
-			<td>宅配回数<br><span class="requiredArea">【必須】</span></td>
-			<td>
-				<select name="cycle" id="宅配回数">
-					<option value="" selected="">選択してください</option>
-					@foreach ($cycles as $cycle)
-					<option value="{{{ $cycle['key'] }}}">{{{ $cycle['value'] }}}</option>
-					@endforeach
-				</select>
-			</td>
-		</tr>
-		<tr>
-			<td>お支払方法<br><span class="requiredArea">【必須】</span></td>
-			<td>
-				<select name="payment" id="お支払方法">
-					<option value="">選択してください</option>
-					@foreach ($payments as $payment)
-					<option value="{{{ $payment['key'] }}}" @if ($payment['default'])selected="selected"@endif>{{{ $payment['value'] }}}</option>
-					@endforeach
-				</select>
-			</td>
-		</tr>
-		<tr>
-			<td>配達希望日</td>
-			<td>第一希望日
-				<p>
-					<input type="text" size="10" name="first_choice" autocomplete="on" value="">
-					例：8月7日<br>
-					<br>
-					第二希望日
-					</p><p><input type="text" size="10" name="second_choice" autocomplete="on" value="">
-					例：8月10日<br><br>
-					第三希望日
-					</p><p><input type="text" size="10" name="third_choice" autocomplete="on" value="">
-					例：8月20日<br><br>
-				</p>
-			</td>
-		</tr>
-		<tr>
-			<td>配達時間帯</td>
-			<td>
-				<select name="time" id="配達時間帯">
-					<option value="" selected="">選択してください</option>
-					@foreach ($times as $time)
-					<option value="{{{ $time['key'] }}}">{{{ $time['value'] }}}</option>
-					@endforeach
-				</select>
-			</td>
-		</tr>
-		<tr>
-			<td>お名前<br><span class="requiredArea">【必須】</span></td>
-			<td>
-				<p id="checkText1">
-					<input type="text" size="20" name="name01" autocomplete="on" value="">
-					<input type="text" size="20" name="name02" autocomplete="on" value=""><br>
-				</p>例：山田太郎
-			</td>
-		</tr>
-		<tr>
-			<td>お名前（フリガナ）<br><span class="requiredArea">【必須】</span></td>
-			<td>
-				<p id="checkText2">
-					<input type="text" size="20" name="kana01" autocomplete="on" value="">
-					<input type="text" size="20" name="kana02" autocomplete="on" value=""><br>
-				</p>例：ヤマダ　タロウ
-			</td>
-		</tr>
-		<tr>
-			<td>住所<br><span class="requiredArea">【必須】</span></td>
-			<td>
-				<p id="checkText3">郵便番号<br>
-					<input type="text" class="js-zip1" size="10" name="zip01" id="郵便番号1" autocomplete="on" value="">&nbsp;-&nbsp;
-					<input type="text" class="js-zip2" size="10" name="zip02" id="郵便番号2" autocomplete="on" value=""><br>
-					<input type="button" class="js-zip2add" value="Zip 2 Address">
-				</p>
-				<br>
-				<p id="checkText4">住所<br>
-					<select name="pref" class="js-pref">
-						<option value="">選択してください</option>
-						@foreach ($prefs as $code => $name)
-						<option value="{{{ $code }}}">{{{ $name }}}</option>
-						@endforeach
-					</select>
-					<br />
-					<input type="text" size="40" name="addr01" autocomplete="on" value="" class="js-addr01"><br />
-					<input type="text" size="40" name="addr02" autocomplete="on" value="" class="js-addr02"><br />
-				</p>例：福岡県福岡市中央春吉1-14-5 春吉マンション101
-			</td>
-		</tr>
-		<tr>
-			<td>電話番号<br><span class="requiredArea">【必須】</span></td>
-			<td>
-				<p id="checkText5">
-					<input type="text" size="10" name="tel01" autocomplete="on" value=""> -
-					<input type="text" size="10" name="tel02" autocomplete="on" value=""> -
-					<input type="text" size="10" name="tel03" autocomplete="on" value=""><br>
-				</p>例：0312345678
-			</td>
-		</tr>
-		<tr>
-			<td>メールアドレス<br><span class="requiredArea">【必須】</span></td>
-			<td>
-				<p id="checkText6">
-					<input type="text" size="40" name="email" autocomplete="on" value=""><br>
-					<br>確認のため、もう一度入力してください。<br>
-					<input type="text" size="40" name="email_confirmation" value=""><br>
-				</p>
-			</td>
-		</tr>
-		<tr>
-			<td>その他</td>
-			<td>
-				<textarea cols="50" rows="5" name="other"></textarea>
-				<p>※1,000文字以内で入力してください。</p>
-			</td>
-		</tr>
-	</tbody>
-</table>
-<input type="button" class="js-submit" value="Send">
-{{ Form::close() }}
+<div class="lpMain">
+	<div class="lpYasai01">
+		<img src="/images/lp/bg_top.png" width="950" height="54">
+		<div class="lpYasai01In clearfix">
+			<img src="/images/lp/lpImg02.png" width="279" height="91" alt="野菜紹介小松菜">
+			<img src="/images/lp/yasai01.png" width="381" height="501" class="img03R" alt="小松菜画像">
+			<h2>カロテン・カルシウムが豊富</h2>
+			<img src="/images/lp/lpImg04.png" width="309" height="94" alt="小松菜の特徴">
+			<p>中国原産で江戸時代に東京の小松川地区で栽培された「小松菜」。アブラナ科の野菜なので、寒さに強く霜に当たると甘みが増すため、冬（12月から2月）が旬となります。</p>
+			<p>欧州でも[komatsuna]と呼ばれ、ベビーリーフとしてもスーパーに並んでいます。
+			カルシウム・鉄分・ビタミン・食物繊維といった栄養が豊富で、コレステロール値を下げるクロロフィルも含まれています。</p>
+			<p>またビタミンKを含んでいるため、骨粗しょう症予防にもなる非常に栄養成分豊富な野菜です。</p>
+			<img src="/images/lp/lpImg05.png" width="348" height="72" alt="小松菜の調理ポイント">
+			<p class="lpPoint">アクが少なく生食も可能なので、サラダとしてそのまま食べれば、水溶性の栄養素を逃すことなく摂取できます。また、タンパク質や油脂を含む食品と組み合わせることで、カルシウムを効率よく吸収することもできます。</p>
+		</div>
+		<img src="/images/lp/bg_bottom.png" width="950" height="54">
+	</div>
+	<!--/ lpYasai01-->
 
-@include('shares/footer')
+		<div class="lpYasai01">
+			<img src="/images/lp/bg_top2.png" width="950" height="54">
+			<div class="lpYasai02In clearfix">
+			<img src="/images/lp/lpImg10.png" width="337" height="88" alt="野菜紹介人参">
+			<img src="/images/lp/yasai02.png" width="381" height="366" class="img03R" alt="ニンジン画像">
+			<h2>葉っぱや皮ごと食べて、栄養を余さず食べよう</h2>
+			<img src="/images/lp/lpImg11.png" width="337" height="88" alt="にんじんの特徴">
+			<p>世界中で家庭料理に利用させる人参は、日本でも、サラダ・炒めもの・煮物・ジュースなどなど様々な料理に活用されています。</p>
+			<p>にんじんにはカロテンがたっぷり。カロテンは皮膚や粘膜を健康に保つ働きをし、また免疫力を高め、心臓病や動脈硬化にも効果もあります。
+			カロテンは特に皮の下に多く含まれいるので、皮はできるだけ薄くむくことをオススメします。 </p>
+			<p>また、パパのにんじんは葉つきのにんじんです。
+			人参葉は香りがよくビタミンCやカルシウムが豊富なので、捨てずに天ぷらや炒めものにしてください。</p>
+			<img src="/images/lp/lpImg05.png" width="348" height="72" alt="にんじんの調理ポイント">
+				<p class="lpPoint">人参にたくさん含まれるカロチンは油に溶けるビタミンです。油と一緒に取ることでビタミンＡとしての吸収率が高くなります。また、生でも食べられるにんじんですが、にんじんにはアスコルビナーゼというビタミンCを壊す酵素が含まれています。生で食べるときには酢やレモン汁をかけることでこの酵素を働きを抑制することができます。</p>
+		</div>
+		<img src="/images/lp/bg_bottom2.png" width="950" height="54">
+	</div>
+	<!--/ lpYasai01-->
+
+	<div class="lpYasai01">
+		<img src="/images/lp/bg_top.png" width="950" height="54">
+		<div class="lpYasai01In clearfix">
+		<img src="/images/lp/lpImg07.png" width="250" height="89" alt="野菜紹介オクラ">
+		<img src="/images/lp/yasai03.png" width="381" height="393" class="img03R" alt="オクラ画像">
+		<h2>ネバネバを食べて免疫系を整えよう</h2>
+		<img src="/images/lp/lpImg06.png" width="321" height="61" alt="おくらの特徴">
+		<p>北アフリカ原産でエジプトでは紀元前から栽培されていたと言われるオクラ。</p>
+		<p>あのネバネバの正体は、タンパク質のムチン・食物繊維のペクチンやガラクタンなどです。ペクチンは血中コレステロールをへらし、血圧を下げる効果があります。</p>
+		<p>また、ムチンには胃の粘膜保護し、消化促進・整腸などの効果があります。
+		また、カロテン・ビタミン・カリウム・カルシウムなども多く含まれ、体の免疫系を整えることができます。</p>
+		<img src="/images/lp/lpImg05.png" width="348" height="72" alt="小松菜の調理ポイント">
+		<p class="lpPoint">表面の産毛が残っていると食感がよくありません。塩をふり、まな板にこすりつけるようにすり合わせ、産毛を取ってください。また、下茹でする場合は茹で過ぎに注意してください。ぬめりと栄養をが抜けてしまうため、少し固めにさっと茹でるのがポイントです。</p>
+		</div>
+		<img src="/images/lp/bg_bottom.png" width="950" height="54">
+	</div>
+	<!--/ lpYasai01-->
+
+		<div class="lpYasai01">
+			<img src="/images/lp/bg_top2.png" width="950" height="54">
+			<div class="lpYasai02In clearfix">
+			<img src="/images/lp/lpImg08.png" width="336" height="92" alt="野菜紹介みょうが">
+			<img src="/images/lp/yasai04.png" width="381" height="363" class="img03R" alt="みょうが画像">
+			<h2>みょうがを食べ過ぎるとバカになる？そんなことはありません。</h2>
+			<img src="/images/lp/lpImg09.png" width="330" height="85" alt="みょうがの特徴">
+			<p>薬味としてお馴染みのみょうが。香り成分はαピネンという精油成分で、食欲増進・消化促進・血行促進などの硬化があります。</p>
+			<p>また、辛味成分には強い抗菌作用があり、発熱による体の不調を調えたり、解毒作用・喉の痛みなどへの効果が期待されます。</p>
+			<p>ちなみに、みょうがを食べ過ぎると馬鹿になるという逸話があります。これは、物忘れが激しい釈迦の弟子の一人の死後、お墓にみょうががたくさん生えていたことによるものです。なので、実際にみょうがを食べ過ぎると忘れっぽくなるというわけではありません。</p>
+			<img src="/images/lp/lpImg05.png" width="348" height="72" alt="にんじんの調理ポイント">
+			<p class="lpPoint">薬味として、またそのまま酢漬けにして食べます。アクが強いので、水にさらしてアクを抜きを行いますが、さらし過ぎると香りと辛味が抜けてしまいますので、さっと行ってください。</p>
+		</div>
+		<img src="/images/lp/bg_bottom2.png" width="950" height="54">
+	</div>
+	<!--/ lpYasai01-->
+
+	<div class="lpPack">
+		<a href="form"><img src="/images/lp/teikiBtn2.png" width="831" height="134" class="teikiBtn2"></a>
+	</div>
+	<div class="lpFeature">
+		<img src="/images/lp/feature01.png" width="980" height="103">
+		<img src="/images/lp/feature03.png" width="924" height="231" class="feature03">
+		<img src="/images/lp/feature04.png" width="980" height="1281">
+		<img src="/images/lp/feature02.png" width="980" height="103">
+		<img src="/images/lp/feature05.png" width="980" height="258" class="feature05">
+		<img src="/images/lp/feature06.png" width="980" height="1082">
+	</div>
+	<div class="lpPack">
+		<a href="#form"><img src="/images/lp/teikiBtn2.png" width="831" height="134" class="teikiBtn2"></a>
+	</div>
+</div>
+<!--/ lpMain -->
+
+<div class="lpMain2">
+	<div class="hoba">
+		<h2>ほ場整備・企業農園もサポートしています</h2>
+		<p class="hoba01">農園新設工事のプロデュース！</p>
+		<p class="hoba02">私たちは、お客様の要望を受け、農園新設工事のプロデュースを行っています。
+		私たちが開発したマクロバイオテックガーデンシステムでは、できるだけほ場付近（半径20キロ圏内）で手に入る資材を積極的に利用し、生態系を維持しています。
+		また、このシステムでは土作りに注力しています。土作りをしっかり行った畑は、その後の管理の手間が格段に減るため、経験のない方々でも農園の運営が可能となります。</p>
+	</div>
+	<h2 class="flowH2">お申込みからお届けまでの流れ</h2>
+	<ul class="flowList clearfix">
+		<li><p class="flowList01">フォームより注文</p><p class="flowList02">下記のご注文フォームにご記入のうえ、お申込みください。</p><img src="/images/lp/flow01.png" width="198" height="138"></li>
+		<li><p class="flowList01">野菜の収穫</p><p class="flowList02">その時期で最も良いお野菜をピックアップし、在庫を確保します。</p><img src="/images/lp/flow02.png" width="198" height="138"></li>
+		<li><p class="flowList01">ご自宅へ宅配</p><p class="flowList02">ご希望の日程に合わせて、採りたて野菜を梱包・発送致します。</p><img src="/images/lp/flow03.png" width="198" height="138"></li>
+	</ul>
+	<h2 class="payH2">宅配回数・お支払方法について</h2>
+	<div class="lpPay">
+		<h3>■選べる宅配回数</h3>
+		<p>PAPA野菜直送便は、お試しでもお申込みできますが、定期便にも対応しております。お試し注文の後、定期便に切り替えたい場合は、お申込みよりご連絡ください。宅配回数は、<span class="red">「初回お試し1回のみ」「毎週」「月2回」「月1回」</span>となっております。</p>
+		<h3 class="lpPayH3">■お支払方法</h3>
+		<p>現在、<span class="red">料金代引きのみ</span>の対応となっております。</p>
+	</div>
+	<div class="lPTable">
+		<img src="/images/lp/lpForm_top.png" width="876" height="111">
+		<div class="lPTableIn">
+			{{ Form::open(['url' => '/company/contact/confirm', 'class' => 'js-register-form']) }}
+			<table>
+				<tbody>
+					<tr>
+						<td class="tdLeft">ご希望のセット<br /><span class="requiredArea">【必須】</span></td>
+						<td class="tdRight">
+							<select name="set">
+								<option value="" selected="">選択してください</option>
+								@foreach ($sets as $set)
+								<option value="{{{ $set['key'] }}}">{{{ $set['value'] }}}</option>
+								@endforeach
+							</select>
+							<p class="errorMsg js-error-set"></p>
+						</td>
+					</tr>
+					<tr>
+						<td>宅配回数<br /><span class="requiredArea">【必須】</span></td>
+						<td>
+							<select name="cycle">
+								<option value="" selected="">選択してください</option>
+								@foreach ($cycles as $cycle)
+								<option value="{{{ $cycle['key'] }}}">{{{ $cycle['value'] }}}</option>
+								@endforeach
+							</select>
+							<p class="errorMsg js-error-cycle"></p>
+						</td>
+					</tr>
+					<!--
+					<tr>
+						<td>お支払方法<br><span class="requiredArea">【必須】</span></td>
+						<td>
+							<select name="payment" id="お支払方法">
+								<option value="">選択してください</option>
+								@foreach ($payments as $payment)
+								<option value="{{{ $payment['key'] }}}" @if ($payment['default'])selected="selected"@endif>{{{ $payment['value'] }}}</option>
+								@endforeach
+							</select>
+						</td>
+					</tr>
+					-->
+					<tr>
+						<td>配達希望日</td>
+						<td>
+							<p class="mb15">
+								第一希望日<br />
+								<input type="text" size="10" name="first_choice">
+								例：8月7日<br />
+								<span class="errorMsg js-error-first_choice"></span>
+							</p>
+							<p class="mb15">
+								第二希望日<br />
+								<input type="text" size="10" name="second_choice">
+								例：8月10日<br />
+								<span class="errorMsg js-error-second_choice"></span>
+							</p>
+							<p>
+								第三希望日<br />
+								<input type="text" size="10" name="third_choice">
+								例：8月20日<br />
+								<span class="errorMsg js-error-third_choice"></span>
+							</p>
+						</td>
+					</tr>
+					<tr>
+						<td>配達時間帯</td>
+						<td>
+							<select name="time">
+								<option value="" selected="">選択してください</option>
+								@foreach ($times as $time)
+								<option value="{{{ $time['key'] }}}">{{{ $time['value'] }}}</option>
+								@endforeach
+							</select>
+							<p class="errorMsg js-error-time"></p>
+						</td>
+					</tr>
+					<tr>
+						<td>お名前<br><span class="requiredArea">【必須】</span></td>
+						<td>
+							<p>
+								<input type="text" size="10" name="name01" placeholder="性">
+								<input type="text" size="10" name="name02" placeholder="名"><br>
+							</p>
+							<p class="errorMsg js-error-name01"></p>
+							<p class="errorMsg js-error-name02"></p>
+							<span>例：山田 太郎</span>
+						</td>
+					</tr>
+					<tr>
+						<td>お名前（フリガナ）<br><span class="requiredArea">【必須】</span></td>
+						<td>
+							<p>
+								<input type="text" size="10" name="kana01" placeholder="セイ">
+								<input type="text" size="10" name="kana02" placeholder="メイ"><br>
+							</p>
+							<p class="errorMsg js-error-kana01"></p>
+							<p class="errorMsg js-error-kana02"></p>
+							<span>例：ヤマダ タロウ</span>
+						</td>
+					</tr>
+					<tr>
+						<td>郵便番号<br /><span class="requiredArea">【必須】</span></td>
+						<td>
+								<input type="text" class="js-zip1" size="10" name="zip01">&nbsp;-&nbsp;
+								<input type="text" class="js-zip2" size="10" name="zip02">
+								<input type="button" class="js-zip2add" value="郵便番号から住所を入力">
+								<br />
+								<p class="errorMsg js-error-zip01 js-error-zip02"></p>
+								<span>例：810-0003</span>
+						</td>
+					</tr>
+					<tr>
+						<td>住所<br /><span class="requiredArea">【必須】</span></td>
+						<td>
+							<select name="pref" class="mb5 js-pref">
+								<option value="">選択してください</option>
+								@foreach ($prefs as $code => $name)
+								<option value="{{{ $code }}}">{{{ $name }}}</option>
+								@endforeach
+							</select>
+							<br />
+							<input type="text" size="40" name="addr01" class="mb5 js-addr01" placeholder="市区町村 番地"><br />
+							<input type="text" size="40" name="addr02" class="js-addr02" placeholder="マンション名"><br />
+							<p class="errorMsg js-error-pref"></p>
+							<p class="errorMsg js-error-addr01"></p>
+							<p class="errorMsg js-error-addr02"></p>
+							<span>例：福岡県福岡市中央春吉1-14-5 春吉マンション101</span>
+						</td>
+					</tr>
+					<tr>
+						<td>電話番号<br><span class="requiredArea">【必須】</span></td>
+						<td>
+							<p>
+								<input type="text" size="10" name="tel01"> -
+								<input type="text" size="10" name="tel02"> -
+								<input type="text" size="10" name="tel03"><br>
+							</p>
+							<p class="errorMsg js-error-tel01 js-error-tel02 js-error-tel03"></p>
+							<span>例：092-737-7311</span>
+						</td>
+					</tr>
+					<tr>
+						<td>メールアドレス<br><span class="requiredArea">【必須】</span></td>
+						<td>
+							<input type="text" size="40" name="email"><br>
+							<br>確認のため、もう一度入力してください。<br>
+							<input type="text" size="40" name="email_confirmation" value=""><br>
+							<p class="errorMsg js-error-email"></p>
+						</td>
+					</tr>
+					<tr>
+						<td>その他</td>
+						<td>
+							<textarea cols="50" rows="5" name="other"></textarea>
+							<p class="errorMsg js-error-other"></p>
+							<p>※1,000文字以内で入力してください。</p>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2" align="center">
+							<input type="button" class="botton js-submit" value="申し込む">
+							<div id="loading"></div>
+							<p class="finishMsg" style="display: none;">申し込み処理が完了いたしました。
+							<p class="finishMsg" style="display: none;">ありがとうございました。</p>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			{{ Form::close() }}
+		</div>
+		<img src="/images/lp/lpForm_bottom.png" width="876" height="30">
+	</div>
+	<div class="lpcCopy">
+		<p>ご不明な点など、お問い合わせは<a href="/company/contact" target="_blank">こちら</a>から</p>
+		<p>Copyright © <a href="/">Organic-papa</a> All rights reserved.</p>
+	</div>
+</div>
+<!--/lpMain2-->
+
+<p id="back-top">
+	<a href="#top"><span></span>&nbsp;</a>
+</p>
 
 @stop
 
 @section('end_script')
+<script src="/js/vendors/jquery.activity-indicator-1.0.0.min.js"></script>
 <script type="text/javascript">
 $(function() {
 	var token = "{{{ csrf_token() }}}";
 
 	$('.js-zip2add').on('click', function() {
+		$('.js-error-zip01').empty();
+
+		var zip1 = $('.js-zip1').val();
+		var zip2 = $('.js-zip2').val();
+		if (!zip1 || !zip2) {
+			$('.js-error-zip01').text('郵便番号を入力してください');
+			return false;
+		}
+
+		var regex = /[0-9]+/;
+		if (!regex.test(zip1) || !regex.test(zip2)) {
+			$('.js-error-zip01').text('郵便番号を正しく入力してください');
+			return false;
+		}
+
 		var data = {
 			'_token': token,
-			'zipcode': $('.js-zip1').val() + $('.js-zip2').val()
+			'zipcode': zip1 + zip2
 		};
 
 		$.ajax({
@@ -161,11 +347,25 @@ $(function() {
 			$('.js-pref').val(data.code);
 			$('.js-addr01').val(data.city + data.town);
 		}).fail(function(data) {
-			console.log(data);
+			$('.js-error-zip01').text('住所の自動入力に失敗しました');
 		});
 	});
 
 	$('.js-submit').on('click', function() {
+		var $button = $(this);
+		var $loading = $('#loading');
+
+		$('.errorMsg').empty();
+		$button.hide();
+		$loading.show().css({padding: '20px'}).activity({
+			segments: 12,
+			width: 6,
+			space: 4,
+			length: 1,
+			color: '#000',
+			speed: 1.5
+		});
+
 		var data = {};
 		var $form = $('.js-register-form');
 		$form.find(':input').each(function(elem) {
@@ -177,13 +377,27 @@ $(function() {
 			'url': '/regular/register',
 			'data': data
 		}).done(function(msg, st, xhr) {
-			console.log('done');
-			console.log(msg, st);
+			$loading.hide().css({padding: '0'});
+			$('.finishMsg').show();
 		}).fail(function(xhr, st, error) {
-			console.log('fail');
-			console.log(xhr, st, error);
-			var msg = eval('(' + xhr.responseText + ')');
-			console.log(msg);
+			var position = 0;
+
+			var res = eval('(' + xhr.responseText + ')');
+			_.each(res.messages, function(val, key) {
+				var $err = $('.js-error-' + key);
+				$err.text(val[0]);
+
+				if (position == 0 && $err.length > 0) {
+					position = $err.offset().top - 50;
+				}
+			});
+
+			if (position != 0) {
+				$('html,body').animate({ scrollTop: position }, 'fast');
+			}
+
+			$loading.hide().css({padding: '0'});
+			$button.show();
 		});
 	});
 
