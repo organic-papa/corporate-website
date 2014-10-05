a146df320d713a3bed85a4155cbe3ee421bf3eba
Keita Goto <lw.mjw.kok@gmail.com>
2014-10-06 02:56:54 +0900

    google analyticsのトラッキングスクリプトを埋め込む

--- app/config/const.php
+++ app/config/const.php
@@ -6,6 +6,9 @@ return [
 		'papa_info' => 'info@organic-papa.com',
 		'papa_takuhai' => 'takuhai@organic-papa.com',
 	],
+	'analytics' => [
+		'tracking_id' => '',
+	],
 	'eccube' => [
 		'auth_magic' => 'drousliouowriadraethouphacroumauegeatouh',
 		'password_hash_algos' => 'sha256',
